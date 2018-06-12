12829efaa601ae4826bbcb77a0d399f3c2ee4b56
Mushinako <ridoedee@gmail.com>
2018-06-11 21:31:35 -0700

    Debug
    Debugged enough for it to actually function

--- src/js/Find_Pattern.js
+++ src/js/Find_Pattern.js
@@ -1,11 +1,13 @@
 class Toss {
     constructor(arr) {
         this.stack = arr;
+        this.maxball = Math.max(...this.stack);
         this.siteswap = this.stack2Siteswap();
+        this.ss_length = this.siteswap.length;
     }
     stack2Siteswap() {
         var tbs = [];
-        for (let i = 0; i < Math.max(...this.stack); i++) {
+        for (let i = 0; i < this.maxball; i++) {
             tbs.push(i);
         }
         var tbso = tbs.slice(0);
@@ -57,6 +59,7 @@ function sameNumberArray(arr1, arr2) {
     }
 }
 function juggle(b, p) {
+    // Get all ball^period tosses
     var psbl = [];
     for (let i = 0; i < Math.pow(b, p); i++) {
         let tmp = [];
@@ -69,10 +72,84 @@ function juggle(b, p) {
     }
     return psbl;
 }
-function main() {
-    var ball = 4;
-    var period = 6;
+function filterByPeriod(psbl, target) {
+    var res = [];
+    for (let x of psbl) {
+        if (x.ss_length === target) {
+            res.push(x);
+        }
+    }
+    return res;
+}
+function filterRotationaryDuplicates(psbl) {
+    var res = [];
+    var counted = [];
+    for (let x of psbl) {
+        let y = x.siteswap.slice(0);
+        if (!counted.some((cur) => sameNumberArray(cur, y))) {
+            let l = x.ss_length;
+            for (let i = 0; i < l; i++) {
+                let j = y.shift();
+                y.push(j);
+                counted.push(y.slice(0));
+            }
+            res.push(x);
+        }
+    }
+    return res;
+}
+function sortBySiteswap(c) {
+    c.sort((a, b) => {
+        let ass = a.siteswap;
+        let bss = b.siteswap;
+        if (ass.length !== bss.length) {
+            return ass.length - bss.length;
+        }
+        else {
+            for (let i = 0; i < ass.length; i++) {
+                if (ass[i] !== bss[i]) {
+                    return ass[i] - bss[i];
+                }
+            }
+            throw new RangeError("There should not be a match!");
+        }
+    });
+    return c;
+}
+function groupByBall(c) {
+    throw new Error("Not implemented yet");
+}
+function calc(ball, period, ss_sort = true, group_ball = false) {
     var start = Date.now();
     var possibilities = juggle(ball, period);
+    possibilities = filterByPeriod(possibilities, period);
+    if (ss_sort) {
+        try {
+            possibilities = sortBySiteswap(possibilities);
+        }
+        catch (_) {
+            return ['', [], false];
+        }
+    }
+    possibilities = filterRotationaryDuplicates(possibilities);
+    if (group_ball) {
+        console.log("Grouping");
+        possibilities = groupByBall(possibilities);
+    }
     var end = Date.now();
+    var time = `Calculation time: ${end - start} milliseconds`;
+    return [time, possibilities, true];
+}
+var ball = 4;
+var period = 6;
+var [t, p, e] = calc(ball, period);
+if (e) {
+    console.log(t);
+    console.log(`${p.length} answers:`);
+    for (let x of p) {
+        console.log(x.siteswap);
+    }
+}
+else {
+    console.log("Error");
 }
--- src/ts/Find_Pattern.ts
+++ src/ts/Find_Pattern.ts
@@ -1,13 +1,21 @@
+interface Counter {
+    [len: string]: Array<Toss>;
+}
+
 class Toss {
     public stack: Array<number>;
+    public maxball: number;
     public siteswap: Array<number>;
+    public ss_length: number;
     public constructor(arr: Array<number>) {
         this.stack = arr;
+        this.maxball = Math.max(...this.stack);
         this.siteswap = this.stack2Siteswap();
+        this.ss_length = this.siteswap.length;
     }
     private stack2Siteswap(): Array<number> {
         var tbs: Array<number> = [];
-        for (let i: number = 0; i < Math.max(...this.stack); i++) {
+        for (let i: number = 0; i < this.maxball; i++) {
             tbs.push(i);
         }
         var tbso: Array<number> = tbs.slice(0);
@@ -38,7 +46,7 @@ class Toss {
         var ml: number = ss.length;
         for (let i: number = 1; i <= Math.floor(ml / 2); i++) {
             if (!(ml % i)) {
-                if (ss.every((cur: number, j: number, arr: Array<number>) => j < arr.length-i ? cur === arr[j+i] : true)) {
+                if (ss.every((cur: number, j: number, arr: Array<number>): boolean => j < arr.length - i ? cur === arr[j + i] : true)) {
                     ss = ss.slice(0, i);
                 }
             }
@@ -60,8 +68,9 @@ function sameNumberArray(arr1: Array<number>, arr2: Array<number>): boolean {
     }
 }
 
-function juggle(b: number, p: number): Array<object> {
-    var psbl: Array<object> = [];
+function juggle(b: number, p: number): Array<Toss> {
+    // Get all ball^period tosses
+    var psbl: Array<Toss> = [];
     for (let i: number = 0; i < Math.pow(b, p); i++) {
         let tmp: Array<number> = [];
         let bl: number = i;
@@ -74,10 +83,86 @@ function juggle(b: number, p: number): Array<object> {
     return psbl;
 }
 
-function main(): void {
-    var ball: number = 4;
-    var period: number = 6;
+function filterByPeriod(psbl: Array<Toss>, target: number): Array<Toss> {
+    var res: Array<Toss> = [];
+    for (let x of psbl) {
+        if (x.ss_length === target) {
+            res.push(x);
+        }
+    }
+    return res;
+}
+
+function filterRotationaryDuplicates(psbl: Array<Toss>): Array<Toss> {
+    var res: Array<Toss> = [];
+    var counted: Array<Array<number>> = [];
+    for (let x of psbl) {
+        let y: Array<number> = x.siteswap.slice(0);
+        if (!counted.some((cur: Array<number>): boolean => sameNumberArray(cur, y))) {
+            let l: number = x.ss_length;
+            for (let i: number = 0; i < l; i++) {
+                let j: number = y.shift();
+                y.push(j);
+                counted.push(y.slice(0));
+            }
+            res.push(x);
+        }
+    }
+    return res;
+}
+
+function sortBySiteswap(c: Array<Toss>): Array<Toss> {
+    c.sort((a: Toss, b: Toss): number => {
+        let ass: Array<number> = a.siteswap;
+        let bss: Array<number> = b.siteswap;
+        if (ass.length !== bss.length) {
+            return ass.length - bss.length;
+        } else {
+            for (let i: number = 0; i < ass.length; i++) {
+                if (ass[i] !== bss[i]) {
+                    return ass[i] - bss[i];
+                }
+            }
+            throw new RangeError("There should not be a match!");
+        }
+    });
+    return c;
+}
+
+function groupByBall(c: Array<Toss>): Array<Toss> {
+    throw new Error("Not implemented yet");
+}
+
+function calc(ball: number, period: number, ss_sort: boolean = true, group_ball: boolean = false): [string, Array<Toss>, boolean] {
     var start: number = Date.now();
-    var possibilities: Array<object> = juggle(ball, period);
+    var possibilities: Array<Toss> = juggle(ball, period);
+    possibilities = filterByPeriod(possibilities, period);
+    if (ss_sort) {
+        try {
+            possibilities = sortBySiteswap(possibilities);
+        } catch (_) {
+            return ['', [], false];
+        }
+    }
+    possibilities = filterRotationaryDuplicates(possibilities);
+    if (group_ball) {
+        console.log("Grouping");
+        possibilities = groupByBall(possibilities);
+    }
     var end: number = Date.now();
+    var time: string = `Calculation time: ${end - start} milliseconds`;
+    return [time, possibilities, true];
+}
+
+var ball = 4;
+var period = 6;
+var [t, p, e]: [string, Array<Toss>, boolean] = calc(ball, period);
+if (e) {
+    console.log(t);
+    console.log(`${p.length} answers:`);
+    for (let x of p) {
+        console.log(x.siteswap);
+    }
+} else {
+    console.log("Error");
 }
