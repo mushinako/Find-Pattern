4b6fd01a09231bd41fd33075ba7bd460e3ef4ce3
Mushinako <ridoedee@gmail.com>
2018-10-06 19:38:23 -0700

    Working C++!
    Barely working, but working!

--- src/cpp/Calc_Pattern.cpp
+++ src/cpp/Calc_Pattern.cpp
@@ -1,57 +1,44 @@
+#include <iostream>
+#include <algorithm>
 #include <cmath>
 #include <vector>
 #include <map>
 
 using namespace std;
 
-int sum;
+int sum = 0;
 vector<int> sums;
-vector<int> dividers;
 
 
-map<int, int> factor(int n, map<int, int>& f) {
+map<int, int> factor(int n) {
     map<int, int> factors;
-    if (n == 1) {
-        return;
-    }
+    if (n == 1) return factors;
     int i = 2;
-    while (n > i) {
-        for (;; ++i) {
-            if (!(n % i)) {
-                n /= i;
-                if (factors.find(i) == factors.end()) {
-                    factors[i] = 1;
-                } else {
-                    factors[i]++;
-                }
-                break;
-            }
-            if (i >= floor(sqrt(n))) {
-                goto outta_loop;
-            }
+    while (n > i) for (;; ++i) {
+        if (!(n % i)) {
+            n /= i;
+            if (factors.find(i) == factors.end()) factors[i] = 1;
+            else factors[i]++;
+            break;
         }
+        if (i >= floor(sqrt(n))) goto outta_loop;
     }
 outta_loop:
-    if (factors.find(n) == factors.end()) {
-        factors[n] = 1;
-    } else {
-        factors[n]++;
-    }
+    if (factors.find(n) == factors.end()) factors[n] = 1;
+    else factors[n]++;
     return factors;
 }
 
 
-void divider(int n) {
-    dividers.clear();
+vector<int> divider(int n) {
+    vector<int> dividers;
 
-    if (n == 1) {
-        return;
-    }
+    if (n == 1) return dividers;
 
     map<int, int> fs = factor(n);
     int count = 1;
     vector<int> nums, occs;
-    for (map<int, int>::iterator i = fs.begin(); i != fs.end(); ++i) {
+    for (map<int, int>::const_iterator i = fs.begin(); i != fs.end(); ++i) {
         nums.push_back(i->first);
         occs.push_back(i->second + 1);
         count *= (i->second + 1);
@@ -70,44 +57,39 @@ void divider(int n) {
     }
 
     sort(dividers.begin(), dividers.end());
+
+    return dividers;
 }
 
 
 int mu(int n) {
-    // TODO: mu
     map<int, int> fs = factor(n);
-    for (map<int, int>::iterator i = fs.begin(); i != fs.end(); ++i) {
-        if (i->second > 1) {
-            return 0;
-        }
-    }
+    for (map<int, int>::const_iterator i = fs.begin(); i != fs.end(); ++i) if (i->second > 1) return 0;
     return (fs.size() % 2) ? -1 : 1;
 }
 
 
 void calc(int b, int p) {
-    sum = 0;
-    sums.clear();
-
     if (p == 1) {
         sum = b;
-        vector<int> sums (b, 1);
+        sums.resize(b, 1);
         return;
     }
 
     // \frac{1}{p}\sum_{d|p}{\mu(\frac{p}{d})(b^d-(b-1)^d)} for exact ball
     // \frac{b^d}{p}\sum_{d|p}{\mu(\frac{p}{d})} for max ball
-    vector<int> sums (b, 0);
-    divider(p);
+    sums.resize(b, 0);
+    vector<int> dividers = divider(p);
+
     for (const int &x : dividers) {
-        int m = mu(p / x);              // TODO: mu
+        int m = mu(p / x);
         if (m) {
             sum += m * pow(b, x);
-            for (int i = 0; i < b; i++) {
-                sums[i] += m * (pow(i+1, x) - pow(i, x));
-            }
+            for (int i = 0; i < b; i++) sums[i] += m * (pow(i+1, x) - pow(i, x));
         }
     }
+    sum /= p;
+    for (int &x : sums) x /= p;
     return;
 }
 
@@ -117,6 +99,11 @@ int main(int argc, char const *argv[]) {
     int period = 6;
 
     calc(ball, period);
+
+    cout << "sum: " << sum << "\n";
+    cout << "sums:";
+    for (const int &x : sums) cout << " " << x;
+    cout << "\n";
 
     return 0;
 }
